49d9e3af633c3db40d00d749f0e296f3b1bc9ad8
David Araujo <david2araujo5@gmail.com>
2021-04-28 13:56:40 +0100

    Update

--- G03/prog1.s
+++ G03/prog1.s
@@ -1,16 +1,16 @@
-		.data
-		
-		.equ SFR_BASE_HI, 0xBF88	# 16 MSbits of SFR area
-		.equ TRISE, 0x6100			# TRISE address is 0xBF886100
-		.equ PORTE, 0x6110			# PORTE address is 0xBF886110
-		.equ LATE, 0x6120			# LATE address is 0xBF886120
-		
-		.equ TRISB, 0x6040
-		.equ PORTB, 0x6050
-		.equ LATB, 0x6060
-		
-		.text
-		.globl main
+	.data
+	
+	.equ SFR_BASE_HI, 0xBF88	# 16 MSbits of SFR area
+	.equ TRISE, 0x6100			# TRISE address is 0xBF886100
+	.equ PORTE, 0x6110			# PORTE address is 0xBF886110
+	.equ LATE, 0x6120			# LATE address is 0xBF886120
+	
+	.equ TRISB, 0x6040
+	.equ PORTB, 0x6050
+	.equ LATB, 0x6060
+	
+	.text
+	.globl main
 		
 #	RE0 -> Output	(0)
 #	RB0 -> Input	(1)
@@ -28,7 +28,7 @@ while:	lw	$t2, PORTB($t1)		# Read and clean LATB register (input)
 	andi	$t2, $t2, 0x0001		
 
 	lw	$t3, LATE($t1)		# Read and clean LATE register (output)
-	andi	$t3, $t2, 0xFFFE	
+	andi	$t3, $t3, 0xFFFE	
 
 	or	$t3, $t3, $t2		# Combine both registers and write to LATE
 	sw	$t3, LATE($t1)
