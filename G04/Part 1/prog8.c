4c86e237b7b9cf197eeeef87b3765818ce955d3d
David Araujo <david2araujo5@gmail.com>
2021-04-30 01:04:31 +0100

    Guiao 04

--- G04/Part 1/prog8.c
+++ G04/Part 1/prog8.c
@@ -3,8 +3,8 @@
 int main(void){
 	int display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
 
-	LATDbits.LATD6 = 1; // display high active
-	LATDbits.LATD5 = 0; // display low inactive
+	LATDbits.LATD6 = 0; // display high active
+	LATDbits.LATD5 = 1; // display low inactive
 	TRISDbits.TRISD5 = 0;
 	TRISDbits.TRISD6 = 0;
 	
